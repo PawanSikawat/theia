855e3fa39f100c1519ee067d8d1240c9e49a8e0f
PawanSikawat <35492860+PawanSikawat@users.noreply.github.com>
2023-01-12 00:17:04 +0530

    Delete theia directory

--- theia/models/stack.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-
-class Stack(BaseModel):
-    name: str
-    project: str
-
-
--- theia/models/static.py
+++
@@ -1,3 +0,0 @@
-class StaticClass:
-    def __new__(cls):
-        raise TypeError(f"{cls.__name__} is static. It cannot be instantiated!")
