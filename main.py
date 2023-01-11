f3582de7bba22a6695e85e2a5a4194e8febdfb05
PawanSikawat <35492860+PawanSikawat@users.noreply.github.com>
2023-01-12 00:18:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,34 +12,3 @@ for router in derived_router:
 if __name__ == "__main__":
     uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, workers=2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# cp_obj = cp.Cloudport(name='cloudport-test', bucket_args=None, opts=None)
-# # cp_obj.create()
-
-# stack = auto.create_or_select_stack(
-#     stack_name='theia-api',
-#     project_name='demo',
-#     program=cp_obj.create
-# )
-
-# # result = stack.destroy(on_output=print)
-# result = stack.up(on_output=print)
-
-
